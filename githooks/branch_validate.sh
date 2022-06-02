53726b737a5c1fe3e45d00c30d9584fe7070143f
Aaron Isakov <aaronisakov@gmail.com>
2022-06-02 18:19:30 -0400

    fixed RegEx rule to now allow capital characters

--- githooks/branch_validate.sh
+++ githooks/branch_validate.sh
@@ -3,7 +3,7 @@ LC_ALL=C
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex="^(bug|chore|devops|feat|fix|hotfix|release)\/[a-z0-9._-]+$"
+valid_branch_regex="^(bug|chore|devops|feat|fix|hotfix|release)\/[a-zA-Z0-9._-]+$"
 
 message="Commit failed. Branch names in this project must adhere to this contract: $valid_branch_regex. Please rename your branch and try again."
 
