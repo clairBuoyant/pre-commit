fa90d2b609be3e20f3c218ef89fe54b0b52cc2fd
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-08-03 23:52:42 -0400

    feat: error message now red instead of green

--- githooks/branch_validate.sh
+++ githooks/branch_validate.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 LC_ALL=C
+
+NOCOLOR="\033[0m"
+RED="\033[0;31m"
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 valid_branch_regex="^(bug|chore|devops|feat|fix|hotfix|release)\/[a-zA-Z0-9._-]+$"
 
-message="Commit failed. Branch names in this project must adhere to this contract: $valid_branch_regex. Please rename your branch and try again."
+message="Commit failed. Branch names in this project must follow this pattern: $valid_branch_regex. Please rename your branch and try again."
 
 if [[ ! $local_branch =~ $valid_branch_regex ]]
 then
-    echo "$message"
+    echo ${RED}$message${NOCOLOR}
     exit 1
 fi
 
